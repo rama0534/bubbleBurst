20506adf9f4f6801b7215246d56f5f7101877a77
NayuduRama <sadamramanayudu@gmail.com>
2022-11-09 21:53:20 -0600

    BurstedBubble ArrayList added

--- src/BubbleBurst.java
+++ src/BubbleBurst.java
@@ -31,11 +31,11 @@ public class BubbleBurst extends JFrame{
 
     public void MakeBubbles(MouseEvent e, Graphics g, ArrayList<Point> mouseClicks ){
         if(e.getX() < 20 || e.getY() < 20 || e.getX() > 700 || e.getY() > 680){
-            statusbar.setText("Radius less than 50 please click somewhere else.");
-            JOptionPane.showMessageDialog( circlePanel,"Click 50 away from the boarder and existing bubble(s) to make a Bubble ", "Error", JOptionPane.ERROR_MESSAGE );
+            statusbar.setText("Radius less than 25 please click somewhere else.");
+            JOptionPane.showMessageDialog( circlePanel,"Click 25 away from the boarder and existing bubble(s) to make a Bubble ", "Error", JOptionPane.ERROR_MESSAGE );
         }else {
             if(mouseClicks.size() == 0){
-                g.setColor(Color.blue);
+                g.setColor(Color.green);
                 g.drawOval( e.getX()-diameter/2, e.getY(), diameter, diameter);
                 g.fillOval(e.getX()-diameter/2, e.getY(), diameter, diameter);
                 mouseClicks.add(new Point(e.getX(), e.getY()));
@@ -43,15 +43,14 @@ public class BubbleBurst extends JFrame{
             else {
                 count = 1;
                 for(Point i : mouseClicks){
-                    if(Math.abs(e.getX()-i.getX()) > 200 + round || Math.abs(e.getY()-i.getY()) >200 + round){
+                    if(Math.abs(e.getX()-i.getX()) > 100 + round || Math.abs(e.getY()-i.getY()) >100 + round){
                         count = count+1;
-                        System.out.println(count);
                     }
                 }
                 if(count > mouseClicks.size()){
-                    System.out.println("Size is ,......"+mouseClicks.size());
                     g.setColor(Color.green);
                     g.drawOval( e.getX()-diameter/2, e.getY(), diameter, diameter);
+//                    g.drawRect(e.getX()-diameter, e.getY()-diameter/2, diameter*2, diameter*2);
                     g.fillOval(e.getX()-diameter/2, e.getY(), diameter, diameter);
                     mouseClicks.add(new Point(e.getX(), e.getY()));
                 }
@@ -60,32 +59,38 @@ public class BubbleBurst extends JFrame{
                 }
             }
         }
-        for(Point i: mouseClicks){
-            System.out.println("X....."+i.getX()+".....Y......."+i.getY());
-        }
     }
 
     private class Handlerclass implements MouseListener, MouseMotionListener {
         ArrayList<Point> mouseClicks = new ArrayList<>();
+        ArrayList<Point> burstedBubles = new ArrayList<>();
+
         public void mouseClicked(MouseEvent e) {
             Graphics g = getGraphics();
             statusbar.setText(String.format("Clicked at %d, %d", e.getX(), e.getY()));
             if(gameLevel < 30){
                 statusbarBottom.setText("Easy level");
+
                 if(mouseClicks.size() < 4){
                     MakeBubbles(e, g, mouseClicks );
                 }else {
-                    statusbarBottom.setText("Mouse clicks exceeded");
                     for(Point i : mouseClicks){
                         pointLocation = Math.sqrt(Math.pow((i.getX() - e.getX()), 2) + Math.pow((i.getY() - e.getY()), 2));
                         if(pointLocation < diameter/2){
-                            System.out.println(i);
+                            burstedBubles.add(new Point((int) i.getX(), (int) i.getY()));
+                            System.out.println( burstedBubles.size());
                             g.setColor(Color.WHITE);
                             g.drawOval( i.x-diameter/2, i.y, diameter, diameter);
                             g.fillOval(i.x-diameter/2, i.y, diameter, diameter);
                         }
                     }
+
                 }
+                if(burstedBubles.size() == 4){
+                    System.out.println("Game finish");
+                    JOptionPane.showMessageDialog( circlePanel,"Game Finish", "Information", JOptionPane.INFORMATION_MESSAGE );
+                }
+
 
             }else if(gameLevel < 70){
                 statusbarBottom.setText("Medium level");
