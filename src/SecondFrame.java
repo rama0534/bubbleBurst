101acab4ee1133bb2d94937e8ba3e93f216fa81c
NayuduRama <sadamramanayudu@gmail.com>
2022-11-14 07:40:30 -0600

    BurstBubble class created

--- src/SecondFrame.java
+++ src/SecondFrame.java
@@ -31,17 +31,6 @@ public class SecondFrame extends JFrame{
         circlePanel.addMouseMotionListener((handler));
     }
 
-
-    public void BurstBubble( Graphics g, ArrayList<Point> mouseClicks, ArrayList<Point> burstedBubles, MouseEvent e ){
-        for(Point i : mouseClicks){
-            pointLocation = Math.sqrt(Math.pow((i.getX() - e.getX()), 2) + Math.pow((i.getY() - e.getY()), 2));
-            if(pointLocation < diameter/2){
-                burstedBubles.add(new Point((int) i.getX(), (int) i.getY()));
-                Bubble bubble = new Bubble(i.x, i.y, 50,"#FFFFFF", g);
-            }
-        }
-
-    }
     public void Game(ArrayList<Point> mouseClicks, ArrayList<Point> burstedBubles, MouseEvent e, Graphics g){
         if(mouseClicks.size() < numberOfBubbles){
             MakeBubble makeBubble =  new MakeBubble(e,  g, mouseClicks, statusbar, circlePanel);
